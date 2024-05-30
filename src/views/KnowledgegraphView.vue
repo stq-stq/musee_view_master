38a742b8bc3858375b59a1b3b1d78ce18530cc64
stq <stq18309096963@outlook.com>
2024-05-31 02:39:15 +0800

    5.30错题集

--- src/views/KnowledgegraphView.vue
+++ src/views/KnowledgegraphView.vue
@@ -21,21 +21,127 @@
 
         </div>
         <v-card>
-            <v-card-title class="card-title">
+            <!-- <v-card-title class="card-title">
                 核心素养能力档案
-            </v-card-title>
+            </v-card-title> -->
             
-            <div class="chart-container">
-                <v-chart :data="chartData" :extend="chartE" :style="{height: '300px'}">
-                </v-chart>
-            </div>
+            <v-card class="mx-auto content-card" height="100%" style="display: flex;margin-top: 20px;margin-bottom: 20px;">
+                <div style="background-color: #F7F9F9;border-radius: 16px;margin-left: 20px;">
+                    <!-- <div ref="echartRef1" style="width: 60vw; height: 28vh;"></div> -->
+                    <div ref="echartRef2" style="width: 60vw; height: 60vh;"></div>
+                </div>
+                <div style="margin-left:10px;background-color: #F7F9F9;border-radius: 16px;">
+                    <div ref="echartRef3" style="width: 30vw; height: 60vh;"></div>
+                </div>
+            </v-card>
         </v-card>
     </v-card>
     
 </template>
 <script setup>
-import { ref } from 'vue';
-// import * as echarts from 'echarts';
+import { onMounted,ref } from 'vue';
+import * as echarts from 'echarts';
+
+const echartRef1 = ref(null);
+const echartRef2 = ref(null);
+const echartRef3 = ref(null);
+const days = Array.from({ length: 30 }, (_, i) => i + 1); // 生成1到30的数组表示天数
+const searchData = days.map(() => Math.floor(Math.random() * 20)); // 生成随机搜索量数据
+
+const categories = ['多项式函数', '相遇问题', '一元一次方程', '不等式求解', '一次函数', '平面几何', '圆的性质','根与系数关系','指数与对数','因式分解']; // 错题种类
+const dots = Array.from({ length: 10 }, (_, i) => i + 1);
+const categoryData = dots.map(() => Math.floor(Math.random() * 10)); // 各种类错题数量
+const errorData = [
+    { value: 40, name: '语法错误' },
+    { value: 30, name: '拼写错误' },
+    { value: 20, name: '逻辑错误' },
+    { value: 10, name: '事实错误' }
+];
+
+
+onMounted(() => {
+    const myChart2 = echarts.init(echartRef2.value);
+
+    const option2 = {
+        title: {
+            text: '知识点档案',
+            padding: [30, 20, 30, 30],
+            textStyle: {
+                fontSize: 24
+            },
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        xAxis: {
+            type: 'value'
+        },
+        yAxis: {
+            type: 'category',
+            data: categories
+        },
+        
+        series: [
+            {
+                name: '错题数量',
+                type: 'bar',
+                data: categoryData,
+                emphasis: {
+                    focus: 'series'
+                },
+                barWidth: '50%',
+                itemStyle: {
+                    
+                    normal: {
+                        color: '#5470C6',// 可以设置条形的颜色
+                        barBorderRadius: [10, 10, 10, 10] // 设置左上角和右上角的圆角半径为 10，右下角和左下角的圆角半径为 0
+                    }
+                    
+                }
+            }
+        ]
+    };
+
+    myChart2.setOption(option2);
+    const myChart3 = echarts.init(echartRef3.value);
+
+    const option3 = {
+        title: {
+            text: '错题类型分析',
+            left: 'center',
+            padding: [30, 20, 30, 30],
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: '错题类型',
+                type: 'pie',
+                radius: '50%',
+                data: errorData,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    myChart3.setOption(option3);
+
+});
+
 const name1 = ref('张三');
 const age = ref(20);
 const grade = ref('初一');
@@ -69,6 +175,7 @@ const chartE = ref({
         type: 'value'
     }
 });
+
 const data = () => ({
     name1: '张三',
     age: 20,
@@ -301,6 +408,7 @@ const data = () => ({
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
+    margin-bottom: 20px;
 }
 .text-container {
     width: 350px;
--- src/views/QuestionsCollectionView.vue
+++ src/views/QuestionsCollectionView.vue
@@ -18,12 +18,83 @@
                 </div>
             </v-card>
         </div>
+        <div class="overlay-image">
+            <el-button class="custom-button">
+                <img src="../images/button/1.svg" alt="图片 1" />
+            </el-button>
+            <el-button class="custom-button">
+                <img src="../images/button/2.svg" alt="图片 2" />
+            </el-button>
+            <el-button class="custom-button">
+                <img src="../images/button/3.svg" alt="图片 3" />
+            </el-button>
+            <el-button class="custom-button" @click="() => { show == false; openChat = true;}">
+                <img src="../images/button/4.svg" alt="图片 4" />
+            </el-button>
+        </div>
     </div>
+    <template>
+        <v-dialog v-model="openChat" width="700px">
+            <v-card>
+                <div class="icon">
+                    <img src="../images/button/4.svg" alt="图片 4">
+                    <v-card-text class="title-card">
+                        <span class="title" style="font-size: 20px;">ChatGPT</span>
+                        <span class="title">沐斯慧教AI大模型</span>
+                    </v-card-text>
+                </div>
+                <v-card-text style="margin: 30px 30px 30px 30px;">
+                    周一<br>
+                    <br>
+                    预习（16:00-16:45）<br>
+                    1. <b>内容选择：</b> 针对即将学习的基本初等函数的定义、性质进行预习。特别是对函数概念的理解、函数的图像和性质等。<br>
+                    2. <b>方法建议：</b> 使用图像辅助记忆和理解，如通过对照函数图像来辅助理解函数的增减性、奇偶性等。<br>
+                    <br>
+                    复习（19:00-20:00）<br>
+                    1. <b>内容选择：</b> 对之前学习的函数基础知识进行复习，确保对函数的基本概念（如定义域、值域、单调性）有扎实的理解。
+                    2. <b>方法建议：</b> 通过完成教材提供的习题，重点关注解题中的函数概念运用。
+                    <br>
+                    练习（20:00-21:00）<br>
+                    1. <b>内容选择：</b> 挑战一些与基本初等函数相关的习题，从简单题目开始，渐进到一些综合题。<br>
+                    2. <b>方法建议：</b> 分类练习，先练习单一类型的初等函数题目，逐步过渡到涉及函数综合应用的题目。<br>
+                    <br>
+                    深化（21:00-22:00）<br>
+                    1. <b>内容选择：</b> 尝试解决一些实际问题，如应用函数解决一些简单的生活或数学问题。<br>
+                    2. <b>方法建议：</b> 找寻与生活相关的函数应用实例，如预算规划、简单的物理问题等，通过解决实际问题深化对函数的理解和运用。<br>
+                    <br>
+                    这个计划注重从基础概念的理解到实际应用的能力提升，强调通过大量的练习和应用来加深对函数理解的重要性。同时，也鼓励学生利用网络资源和教材之外的练习资源来丰富学习内容和方法。
+                </v-card-text>
+
+                <div class="textarea-container">
+                    <v-text-field placeholder="Message" :model-value="textValue" variant="solo" rounded
+                        @click="openDialog" class="single-line-textarea"></v-text-field>
+
+                    <!-- 更改 -->
+                    <v-btn  :class="['compact-button', 'icon-button']" icon="mdi-arrow-up-circle"
+                        @click="TextSend" color="#2081C3">
+                        <svg-icon type="mdi" :path="mdiArrowUpCircle" class="expand-icon"></svg-icon>
+                    </v-btn>
+
+
+                    <v-btn :class="['compact-button', 'icon-button']" icon="mdi-arrow-up-circle"
+                        @click="ConversationModel" color="#2081C3">
+                        <svg-icon type="mdi" :path="mdiMicrophone" class="expand-icon"
+                            style="height: 40px;height: 40px;"></svg-icon>
+                    </v-btn>
+                </div>
+
+            </v-card>
+        </v-dialog>
+    </template>
 </template>
+
 
 <script setup>
 import CollectionIndex from '@/components/CollectionIndex.vue';
 import MistakePreviewComponent from '@/components/MistakePreviewComponent.vue';
+import { mdiArrowUpCircle, mdiMicrophone, mdiCog } from '@mdi/js';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { ref } from 'vue';
 const mistakeData = [
     {
         tags: ['综合', '选择'],
@@ -58,8 +129,42 @@ const mistakeData = [
         cardColor: '#D0E4F1',
         imgSrc:'https://i.ytimg.com/vi/uhVnS0axFJY/sddefault.jpg'
     },]
+const openChat = ref(false);
+const show = ref(false);
+function ConversationModel() {
+    ConversationShow.value = true;
+}
+function TextSend() {
+    try {
+        commonGlobalState.warntitle = '小沐正在努力思考~'
+        commonGlobalState.dialogVisible = true;
+        switch (commonGlobalState.chatModel) {
+            case 0:
+                sendDefault(textValue.value);
+                break;
+            case 1:
+                sendGuide(textValue.value);
+                break;
+            case 2:
+                sendMistake(textValue.value);
+                break;
+            case 3:
+                sendFeynman(textValue.value);
+                break;
+            case 4:
+                sendexplanation(textValue.value);
+                break;
+        }
+        textValue.value = ' ';
+        localStorage.setItem('renderedFormula', '');
+    } catch (error) {
+        textValue.value = '';
+        localStorage.setItem('renderedFormula', '');
+    }
+
+}
 </script>
-  
+
 <style scoped>
 .container {
     height: 85vh;
@@ -93,5 +198,62 @@ const mistakeData = [
     flex-direction: row;
     justify-content: space-around;
 }
+.custom-button {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    overflow: hidden;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    background-color: #F7F9F9;
+    margin-left: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.custom-button img {
+    width: 50%;
+    height: 50%;
+    
+}
+.overlay-image {
+    position: fixed;
+    display: flex;
+    flex-direction: row;
+    bottom: 60px;
+    right: 60px;
+
+}
+.icon{
+    margin: 30px 30px 0px 30px;
+    display: flex;
+    flex-direction: row;
+}
+.title-card{
+    display: flex;
+    flex-direction: column;
+}
+.compact-button {
+    margin: 5px;
+    density: compact;
+    elevation: 0;
+    width: 30px;
+    height: 30px;
+}
+
+.icon-button {
+    width: 52px;
+    height: 52px;
+}
+.textarea-container {
+    display: flex;
+    padding: 0 10px 0 30px;
+    width: 95%;
+}
+.expand-icon {
+    height: 49px;
+    width: 48px;
+}
 </style>
-  
